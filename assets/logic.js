1efbb973d76124ce905d968fe8c9c995e8004ee7
unknown <jamesschlader@yahoo.com>
2018-09-14 14:04:25 -0600

    first commit. html shell built. ready to code.

---
+++ assets/logic.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBIhe6Rkp-YxL6yTSeuLf8-hzBbHfYfFRM",
+    authDomain: "train-scheduler-got.firebaseapp.com",
+    databaseURL: "https://train-scheduler-got.firebaseio.com",
+    projectId: "train-scheduler-got",
+    storageBucket: "",
+    messagingSenderId: "858231151122"
+  };
+  firebase.initializeApp(config);
